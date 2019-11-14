b071fdc57ff0dcb82ed796ba3e3e0bfb7d6fe1ee
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-14 03:21:33 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Song.all.select { |s| s.artist == self }
+  end
+  
+  def new_song(name, genre)
+  end
+  
+  def genres
+  end
+  
+end
