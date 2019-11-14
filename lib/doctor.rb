5a0dc68568d28f7e734120049a488857ed1caf16
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-14 03:36:16 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,4 +2,23 @@ class Doctor
   
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def appointments
+    Appointment.all.select { |a| a.doctor == self }
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.map{ |m| m.patient }
+  end
+  
+end
